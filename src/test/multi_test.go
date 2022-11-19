39e9ad15542c6bd6d3b08c3f8cab20832396e2c0
madzonbiesbookstore <madzonbiesbookstore@gmail.com>
2022-11-19 11:31:58 +0900

    UT追加

---
+++ src/test/multi_test.go
@@ -0,0 +1,37 @@
+package multi
+
+import "testing"
+
+func multi(a, b int) int {
+	return a * b
+}
+
+func TestMulti(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "normal",
+			args: args{a: 2, b: 2},
+			want: 4,
+		},
+		{
+			name: "normal2",
+			args: args{a: 4, b: 2},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := multi(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("multi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
